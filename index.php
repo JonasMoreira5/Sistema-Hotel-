06ef1227e43a831b4b7cb85c0af23f0ba09539c2
Jonas Moreira <jonasmoreira076@gmail.com>
2023-09-12 17:54:07 -0300

    refactor: refatorando corpo do projeto

--- index.php
+++ index.php
@@ -12,14 +12,6 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <style>
-    .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-    }
-
     @media (min-width: 768px) {
         .bd-placeholder-img-lg {
             font-size: 3.5rem;
@@ -45,65 +37,25 @@
         vertical-align: -.125em;
         fill: currentColor;
     }
-
-    .nav-scroller {
-        position: relative;
-        z-index: 2;
-        height: 2.75rem;
-        overflow-y: hidden;
-    }
-
-    .nav-scroller .nav {
-        display: flex;
-        flex-wrap: nowrap;
-        padding-bottom: 1rem;
-        margin-top: -1px;
-        overflow-x: auto;
-        text-align: center;
-        white-space: nowrap;
-        -webkit-overflow-scrolling: touch;
-    }
-
-    .btn-bd-primary {
-        --bd-violet-bg: #712cf9;
-        --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
-
-        --bs-btn-font-weight: 600;
-        --bs-btn-color: var(--bs-white);
-        --bs-btn-bg: var(--bd-violet-bg);
-        --bs-btn-border-color: var(--bd-violet-bg);
-        --bs-btn-hover-color: var(--bs-white);
-        --bs-btn-hover-bg: #6528e0;
-        --bs-btn-hover-border-color: #6528e0;
-        --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
-        --bs-btn-active-color: var(--bs-btn-hover-color);
-        --bs-btn-active-bg: #5a23c8;
-        --bs-btn-active-border-color: #5a23c8;
-    }
-
-    .bd-mode-toggle {
-        z-index: 1500;
-    }
     </style>
+
     <!-- Custom styles for this template -->
     <link href="assets/css/sidebars.css" rel="stylesheet">
 </head>
 
 <body>
-    <main class="d-flex flex-nowrap">
-        <!-- <h1 class="visually-hidden">Sidebars examples</h1> -->
+    <div class="d-flex flex-nowrap">
+        <!-- Conteúdo da barra lateral -->
+        
         <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 280px;">
-            <a href="?page=config.php"
-                class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                <svg class="bi pe-none me-2" width="40" height="32">
-                    <use xlink:href="#bootstrap" />
-                </svg>
+            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                <!-- adicionar imagem da logo -->
                 <span class="fs-4">Hotel SI</span>
             </a>
             <hr>
             <ul class="nav nav-pills flex-column mb-auto">
                 <li class="nav-item">
-                    <a href="#" class="nav-link active" aria-current="page" href="?page=config.php">
+                    <a href="?page=index.php" class="nav-link active" aria-current="page" href="?page=config.php">
                         <i class="bi bi-house-door me-2"></i>
                         Home
                     </a>
@@ -151,7 +103,6 @@
                     </ul>
                 </li>
             </ul>
-
             <hr>
             <div class="dropdown">
                 <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
@@ -167,14 +118,15 @@
                     <li><a class="dropdown-item" href="#">Sair</a></li>
                 </ul>
             </div>
-            <div class="b-example-divider b-example-vr"></div>
-    </main>
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
+        </div>
+        <div class="b-example-divider b-example-vr"></div>
+        <script src="assets/js/bootstrap.bundle.min.js"></script>
 
-    <div class="container">
-        <div class="row">
-            <div class="col mt-5">
-                <?php
+        <main class="container">
+            <!-- Conteúdo principal -->
+            <div class="row">
+                <div class="col mt-5">
+                    <?php
           include("config.php");
           switch($_REQUEST["page"]){
               case "novo_cliente":
@@ -228,22 +180,20 @@
                   
               default:
                 ?>
-                <div class="row marcador align-items-center">
-                    <div class="col mx-auto text-center">
-                        <figure class="figure">
-                            <img src="assets\img\logo.png" class="figure-img img-fluid rounded" alt="...">
-                        </figure>
+                    <div class="row marcador align-items-center">
+                        <div class="col mx-auto text-center">
+                            <figure class="figure">
+                                <img src="assets\img\logo.png" class="figure-img img-fluid rounded" alt="...">
+                            </figure>
+                        </div>
                     </div>
-                </div>
-                <?php
+                    <?php
           }
         ?>
+                </div>
             </div>
-        </div>
-    </div>
-
-
-    <script src="assets/js/sidebars.js"></script>
+        </main>
+        <script src="assets/js/sidebars.js"></script>
 </body>
 
 </html>
