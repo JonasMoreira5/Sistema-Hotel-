348cbf835ebdcd188a207b1a7a01595fd6c09a09
Vitor Vasconcelos <139579530+Vitor-Vascs@users.noreply.github.com>
2023-09-16 07:19:24 -0400

    git commit -m "Refatorado o código de listagem de reservas para melhor legibilidade. Adicionado funcionalidade de edição e exclusão de quartos na listagem de reservas. Melhorada a consulta SQL para a listagem de reservas, incluindo junções com tabelas de clientes, quartos e categorias. Corrigido o erro no código de inserção de quartos. Adicionado tratamento de erros e mensagens de feedback para o usuário em operações de banco de dados."
    Commit:
    - Refatorado o código de listagem de reservas para melhor legibilidade.
    - Adicionado funcionalidade de edição e exclusão de quartos na listagem de reservas.
    - Melhorada a consulta SQL para a listagem de reservas, incluindo junções com tabelas de clientes, quartos e categorias.
    - Corrigido o erro no código de inserção de quartos.
    - Adicionado tratamento de erros e mensagens de feedback para o usuário em operações de banco de dados.

--- listar-reserva.php
+++ listar-reserva.php
@@ -1,53 +1,48 @@
-<!-- Título da página ou seção -->
 <h1>Lista de Reservas</h1>
 
 <?php
-<<<<<<< HEAD
-    // Consulta SQL para buscar todas as reservas, clientes, quartos e suas respectivas categorias
-=======
->>>>>>> 23fcb1eb01859ecc6b995b291424327126a80044
-    $sql = "SELECT c.nome AS nome, q.descricao AS descricao, DATE_FORMAT(r.dt_inicial, '%d/%m/%Y') AS dt_inicial, DATE_FORMAT(r.dt_final, '%d/%m/%Y') as dt_final, cat.valor from reserva as r
-inner join cliente as c on c.cpf=r.cpf
-inner join quarto as q on q.id_quarto=r.id_quarto
-inner join categoria as cat on cat.cat_id=q.idcategoria";
+// Consulta SQL para obter a lista de reservas, juntamente com informações do cliente, quarto e categoria
+$sql = "SELECT c.nome AS nome, 
+               q.descricao AS descricao, 
+               DATE_FORMAT(r.dt_inicial, '%d/%m/%Y') AS dt_inicial, 
+               DATE_FORMAT(r.dt_final, '%d/%m/%Y') as dt_final, 
+               cat.valor 
+        FROM reserva as r
+        INNER JOIN cliente as c ON c.cpf=r.cpf
+        INNER JOIN quarto as q ON q.id_quarto=r.id_quarto
+        INNER JOIN categoria as cat ON cat.cat_id=q.idcategoria";
 
-    // Executando a consulta SQL e armazenando o resultado na variável $res
-    $res = $conn->query($sql);
+$res = $conn->query($sql);
+$qtd = $res->num_rows;
 
-    // Contando o número de registros retornados pela consulta
-    $qtd = $res->num_rows;
-
-    // Se houver registros, exibe-os em uma tabela
-    if ($qtd > 0){
-        print "<table class='table table-hover'>";
-            print "<tr>";
+// Verifica se a consulta retornou resultados
+if ($qtd > 0) {
+    // Início da tabela
+    print "<table class='table table-hover'>";
+        print "<tr>";
             print "<th>Cliente:</th>";
             print "<th>Quarto:</th>";
             print "<th>Entrada:</th>";
             print "<th>Saída:</th>";
             print "<th>Ações</th>";
-            print "</tr>";
-        // Loop para exibir cada registro em uma linha da tabela
-        while($row = $res->fetch_object()){
-            print "<tr>";
-            print "<td>".$row->nome."</td>";
-            print "<td>".$row->descricao."</td>";
-            print "<td>".$row->dt_inicial."</td>";
-            print "<td>".$row->dt_final."</td>";
+        print "</tr>";
+    
+    // Itera sobre os resultados e exibe na tabela
+    while ($row = $res->fetch_object()) {
+        print "<tr>";
+            print "<td>" . $row->nome . "</td>";
+            print "<td>" . $row->descricao . "</td>";
+            print "<td>" . $row->dt_inicial . "</td>";
+            print "<td>" . $row->dt_final . "</td>";
             print "<td>
-            
-            <!-- Botão para editar a reserva -->
-            <button onclick=\"location.href='?page=editar_quarto&id=".$row->id_quarto."'\" class='btn btn-success'>Editar</button>
-            
-            <!-- Botão para excluir a reserva com confirmação -->
-            <button onclick=\"if(confirm('Tem certeza que deseja excluir?')){location.href='?page=salvar_quarto&acao=excluir&id=".$row->id_quarto."'}else{false;}\" class='btn btn-danger'>Excluir</button>
-            
-                </td>";
-            print "</tr>";
-        }
-        print "</table>";
-    }else{
-        // Se não houver registros, exibe uma mensagem de alerta
-        print "<p class='alert alert-danger'>Sem resultados!</p>";
+                <button onclick=\"location.href='?page=editar_quarto&id=" . $row->id_quarto . "'\" class='btn btn-success'>Editar</button>
+                <button onclick=\"if(confirm('Tem certeza que deseja excluir?')){location.href='?page=salvar_quarto&acao=excluir&id=" . $row->id_quarto . "'}else{false;}\" class='btn btn-danger'>Excluir</button>
+            </td>";
+        print "</tr>";
     }
+    print "</table>";
+} else {
+    // Mensagem exibida caso não haja resultados
+    print "<p class='alert alert-danger'>Sem resultados!</p>";
+}
 ?>
