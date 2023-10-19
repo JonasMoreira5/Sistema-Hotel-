9c884b5de3403142306fee1968aba455b3c39bdc
Jonas Moreira <jonasmoreira076@gmail.com>
2023-10-19 01:29:52 -0300

    refactor:  add de proteção contra injeção de SQL

--- editar-cliente.php
+++ editar-cliente.php
@@ -1,48 +1,50 @@
 <h1>Editar Cliente</h1>
 
 <?php
-    // Consulta SQL para buscar o cliente pelo CPF fornecido
-    $sql = "SELECT * FROM cliente WHERE cpf=" . $_REQUEST["id"];
+// Verifica se existe um cliente válido com o CPF informado
+$cpf = $_REQUEST["id"];
+$sql = "SELECT * FROM cliente WHERE cpf=?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $cpf);
+$stmt->execute();
+$result = $stmt->get_result();
 
-    // Executando a consulta e armazenando o resultado
-    $res = $conn->query($sql);
-
-    // Buscando o objeto do cliente do resultado
-    $row = $res->fetch_object();
+if ($result->num_rows == 0) {
+    echo "Cliente não encontrado";
+    print "<script>alert('Cliente não encontrado')</script>";
+} else {
+    $row = $result->fetch_object();
 ?>
 
-<!-- Formulário para editar o cliente -->
-<form action="?page=salvar_cliente" method="POST">
-    <!-- Campo oculto para armazenar a ação de edição -->
-    <input type="hidden" name="acao" value="editar">
-    
-    <!-- Campo oculto para armazenar o CPF do cliente -->
-    <input type="hidden" name="id" value="<?php print $row->cpf; ?>">
+    <!-- Formulário para editar o cliente -->
+    <form action="?page=salvar_cliente" method="POST">
+        <!-- Campos ocultos para a ação e ID do cliente -->
+        <input type="hidden" name="acao" value="editar">
+        <input type="hidden" name="id" value="<?php echo htmlspecialchars($row->cpf); ?>">
 
-    <!-- Campo para editar o CPF do cliente -->
-    <div class="mb-3">
-        <label>CPF:</label>
-        <input type="text" name="cpf" value="<?php print $row->cpf; ?>" class="form-control">
-    </div>
+        <!-- Campos editáveis ​​para CPF, nome, telefone e e-mail -->
+        <div class="mb-3">
+            <label>CPF:</label>
+            <input type="text" name="cpf" value="<?php echo htmlspecialchars($row->cpf); ?>" class="form-control">
+        </div>
 
-    <!-- Campo para editar o nome do cliente -->
-    <div class="mb-3">
-        <label>Nome:</label>
-        <input type="text" name="nome" value="<?php print $row->nome; ?>" class="form-control">
-    </div>
+        <div class="mb-3">
+            <label>Nome:</label>
+            <input type="text" name="nome" value="<?php echo htmlspecialchars($row->nome); ?>" class="form-control">
+        </div>
 
-    <!-- Campo para editar o telefone do cliente -->
-    <div class="mb-3">
-        <label>Telefone:</label>
-        <input type="tel" name="contato" value="<?php print $row->contato; ?>" class="form-control">
-    </div>
+        <div class="mb-3">
+            <label>Telefone:</label>
+            <input type="tel" name="contato" value="<?php echo htmlspecialchars($row->contato); ?>" class="form-control">
+        </div>
 
-    <!-- Campo para editar o email do cliente -->
-    <div class="mb-3">
-        <label>Email:</label>
-        <input type="email" name="email" value="<?php print $row->email; ?>" class="form-control">
-    </div>
+        <div class="mb-3">
+            <label>Email:</label>
+            <input type="email" name="email" value="<?php echo htmlspecialchars($row->email); ?>" class="form-control">
+        </div>
 
-    <!-- Botão para salvar as alterações -->
-    <button type="submit" class="btn btn-primary">Salvar</button>
-</form>
+        <!-- Botão para salvar as alterações -->
+        <button type="submit" class="btn btn-primary">Salvar</button>
+    </form>
+
+<?php } ?>
