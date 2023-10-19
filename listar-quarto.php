49fe6699dcab93e64f928c6d3744f630aed8838f
Jonas Moreira <jonasmoreira076@gmail.com>
2023-10-18 23:15:47 -0300

    refactor: reformatando comando sql

--- listar-quarto.php
+++ listar-quarto.php
@@ -3,7 +3,7 @@
 
 <?php
     // Consulta SQL para buscar todos os quartos e suas respectivas categorias
-    $sql = "SELECT q.descricao as desc_quarto, c.descricao as desc_cat, c.valor from quarto as q inner join categoria as c where idcategoria=cat_id";
+    $sql = "select q.descricao as desc_quarto, c.descricao as desc_cat, c.valor from quarto as q inner join categoria as c where idcategoria=cat_id";
 
     // Executando a consulta SQL e armazenando o resultado na variável $res
     $res = $conn->query($sql);
@@ -29,7 +29,8 @@
             print "<td>
             
             <!-- Botão para editar o quarto -->
-            <button onclick=\"location.href='?page=editar_quarto&id=".$row->id_quarto."'\" class='btn btn-success'>Editar</button>
+            <button onclick=\"location.href='?page=editar_quarto&id=".$row->id_quarto."'\" class='btn btn-success'>Editar</button>            
+    
             
             <!-- Botão para excluir o quarto com confirmação -->
             <button onclick=\"if(confirm('Tem certeza que deseja excluir?')){location.href='?page=salvar_quarto&acao=excluir&id=".$row->id_quarto."'}else{false;}\" class='btn btn-danger'>Excluir</button>
