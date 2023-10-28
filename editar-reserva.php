a05442eae4313d8fe5b1a6b23e596be648e68b9f
Jonas Moreira <jonasmoreira076@gmail.com>
2023-10-28 15:42:29 -0300

    refacto: nova funcionalidade para editar

--- editar-reserva.php
+++ editar-reserva.php
@@ -1,1 +1,68 @@
-<h1>Editar Reserva </h>
+<h1>Editar Reserva </h1>
+
+
+<?php
+// Consulta SQL para obter a lista de reservas, juntamente com informações do cliente, quarto e categoria
+$sql = "SELECT c.nome AS nome, 
+               q.descricao AS descricao, 
+               DATE_FORMAT(r.dt_inicial, '%d/%m/%Y') AS dt_inicial, 
+               DATE_FORMAT(r.dt_final, '%d/%m/%Y') AS dt_final, 
+               cat.valor 
+        FROM reserva AS r
+        INNER JOIN cliente AS c ON c.cpf=r.cpf
+        INNER JOIN quarto AS q ON q.id_quarto=r.id_quarto
+        INNER JOIN categoria AS cat ON cat.id_categoria=q.idcategoria";
+// Executando a consulta e armazenando o resultado
+$res = $conn->query($sql);
+
+// Buscando o objeto da categoria do resultado
+$qtd = $res->num_rows;
+
+?>
+
+<!-- Formulario para editar a reserva -->
+<form action="?page=salvar_reserva" method="post">
+    <input type="hidden" name="acao" value="editar">
+
+    <!-- Campo para Data de Entrada -->
+    <div class="mb-3">
+        <label>Data de Entrada:</label>
+        <input type="date" name="entrada" class="form-control">
+    </div>
+
+    <!-- Campo para Data de Saída -->
+    <div class="mb-3">
+        <label>Data de Saída:</label>
+        <input type="date" name="saida" class="form-control">
+    </div>
+
+    <!-- Dropdown para seleção de Cliente -->
+    <div class="mb-3">
+        <label>Cliente:</label>
+        <select name="cliente" required class="form-control">
+            <option value="" selected>Selecione o cliente</option>
+            <?php while ($row_cliente = $res_cliente->fetch_object()) { ?>
+                <option value="<?php echo $row_cliente->cpf; ?>">
+                    <?php echo $row_cliente->nome; ?>
+                </option>
+            <?php } ?>
+        </select>
+    </div>
+
+
+    <!-- Dropdown para seleção de Quarto -->
+    <div class="mb-3">
+        <label>Quarto:</label>
+        <select name="quarto" required class="form-control">
+            <option value="" selected>Selecione o quarto</option>
+            <?php while ($row_quarto = $res_quarto->fetch_object()) { ?>
+                <option value="<?php echo $row_quarto->id_quarto; ?>">
+                    <?php echo $row_quarto->desc_quarto . ' - ' . $row_quarto->desc_categoria . ' - ' . $row_quarto->valor; ?>
+                </option>
+            <?php } ?>
+        </select>
+    </div>
+
+    <!-- Botão para salvar as alterações -->
+    <button type="submit" class="btn btn-primary">Salvar</button>
+</form>
--- salvar-reserva.php
+++ salvar-reserva.php
@@ -21,6 +21,24 @@
             break;
             
         case 'editar':
+            $entrada = $_POST["entrada"];
+            $saida = $_POST["saida"];
+            $cliente = $_POST["cliente"];
+            $quarto = $_POST["quarto"];
+            
+            //$sql = "UPDATE reserva SET (dt_inicial, dt_final, cpf, id_quarto) VALUES ('{$entrada}','{$saida}','{$cliente}','{$quarto}')";
+            $sql = "UPDATE reserva SET dt_inicial='{$entrada}', dt_final='{$saida}', cpf='{$cliente}', quarto='{$quarto}'  WHERE cpf=".$_REQUEST["id"];
+            
+            $res = $conn->query($sql);
+            
+            if($res == true){
+                print "<script>alert('Reserva atualizada com sucesso!!!');</script>";
+                print "<script>location.href='?page=listar_reserva'</script>";
+            }else{
+                print "<script>alert('Erro ao atualizar reserva!!!');</script>";
+                print "<script>location.href='?page=listar_reserva'</script>";
+            }
+            break;
             
         case 'excluir':
     }
