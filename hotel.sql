9d643d42c4e8d4c52bd8ad9cdb2b1d0555517430
Jonas Moreira <jonasmoreira076@gmail.com>
2023-11-06 15:48:29 -0300

    refactor: adicioando tabeas ao banco

--- hotel.sql
+++ hotel.sql
@@ -1,16 +1,26 @@
 -- Criação da tabela cliente
 CREATE TABLE cliente 
-( 
+(    
     cpf VARCHAR(11) PRIMARY KEY,  -- CPF do cliente como chave primária
     nome VARCHAR(200) NOT NULL,  -- Nome do cliente
     contato VARCHAR(11) NOT NULL,  -- Contato telefônico do cliente
     email VARCHAR(200) NOT NULL  -- Email do cliente
 ); 
 
+-- Criação da tabela Funcionario
+CREATE TABLE Funcionario 
+(   
+    nome VARCHAR(200) NOT NULL,  -- Nome do Funcionario
+    cpf VARCHAR(11) PRIMARY KEY,  -- CPF do Funcionario como chave primária
+    login VARCHAR(255) NOT NULL, -- login do Fucionario
+    senha VARCHAR(255) NOT NULL,  -- senha do Funcionario
+);
+
 -- Criação da tabela categoria
 CREATE TABLE categoria 
 ( 
     id_categoria INT PRIMARY KEY AUTO_INCREMENT,  -- ID da categoria como chave primária e auto-incremento
+    idservico INT; -- Chave estrangeira referenciando a categoria do servico
     descricao VARCHAR(200) NOT NULL,  -- Descrição da categoria
     valor DECIMAL(4,2) NOT NULL  -- Valor associado à categoria
 ); 
@@ -33,6 +43,7 @@ CREATE TABLE reserva
     id_quarto INT,  -- ID do quarto reservado
     CONSTRAINT fk_res_cli FOREIGN KEY (cpf) REFERENCES cliente (cpf),  -- Restrição de chave estrangeira para cliente
     CONSTRAINT fk_res_qua FOREIGN KEY (id_quarto) REFERENCES quarto (id_quarto)  -- Restrição de chave estrangeira para quarto
+
 );
 
 -- TO DO// tabela serviços
@@ -40,4 +51,6 @@ CREATE TABLE servico (
     cod_servico INT PRIMARY KEY AUTO_INCREMENT,
     tipo_servico VARCHAR(100) NOT NULL,
     valor_servico DECIMAL(4,2) NOT NULL
+    cat_id INT -- Chave estrangeira referenciando o servico da categoria
+    CONSTRAINT fk_cat_id FOREIGN KEY (cat_id) REFERENCES categoria (idservico)  -- Restrição de chave estrangeira
 );
